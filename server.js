db0e67d43f0c0f4a2edd9506fde8221218eb1b70
Kaid <kjjackson261@gmail.com>
2021-04-20 23:06:25 -0700

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 //User port
-const PORT = 3000;
+const PORT =  process.env.PORT || 3000;
 
 const app = express();
 
@@ -16,7 +16,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+
+mongoose.connect( process.env.MONOGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
